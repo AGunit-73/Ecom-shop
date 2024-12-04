4c073d60d3002254608bc48da502d5eab4aee5b3
AGunit-73 <143144185+AGunit-73@users.noreply.github.com>
2024-12-04 10:51:13 -0600

    Update route.ts

--- isolora/src/app/api/product/add-items/route.ts
+++ isolora/src/app/api/product/add-items/route.ts
@@ -1,4 +1,36 @@
 
+// import { sql } from "@vercel/postgres";
+// import { NextResponse } from "next/server";
+
+// interface Item {
+//   name: string;
+//   category: string;
+//   description: string;
+//   price: number;
+//   quantity: number;
+//   imageUrl: string;
+//   user_id: number;
+// }
+
+// export async function POST(request: Request) {
+//   const { name, category, description, price, quantity, imageUrl, user_id } = (await request.json()) as Item;
+//   console.log(name, category, description, price, quantity, imageUrl, user_id);
+
+//   try {
+//     const result = await sql`
+//       INSERT INTO items (name, category, description, price, quantity, image_url, user_id)
+//       VALUES (${name}, ${category}, ${description}, ${price}, ${quantity}, ${imageUrl}, ${user_id})
+//       RETURNING *;
+//     `;
+//     return NextResponse.json({ success: true, item: result.rows[0] });
+//   } catch (error) {
+//     console.error("Error adding item to database:", error);
+//     return NextResponse.json({ success: false, message: "Database error" }, { status: 500 });
+//   }
+// }
+
+
+
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
 
@@ -28,3 +60,4 @@ export async function POST(request: Request) {
     return NextResponse.json({ success: false, message: "Database error" }, { status: 500 });
   }
 }
+
